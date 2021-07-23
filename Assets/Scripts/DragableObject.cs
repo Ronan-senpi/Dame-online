e41261dba720c0d3e0a89afeec8d93da8161c31b
Aedrinios <clementclerc2@gmail.com>
2021-07-23 13:59:53 +0200

    synchronize pawn and rigidbody

--- Assets/Scripts/DragableObject.cs
+++ Assets/Scripts/DragableObject.cs
@@ -1,24 +1,34 @@
+using Photon.Pun;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragableObject : MonoBehaviour
+public class DragableObject : MonoBehaviourPun, IPunObservable
 {
     private Vector3 mOffset;
     private float mZCoord;
-
+    private PhotonView pv;
     private Rigidbody rb;
+
+    private void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
 
     private void Start()
     {
-        if(!TryGetComponent(out rb))
+        if (!TryGetComponent(out rb))
         {
             throw new Exception("Cet objet (DragableObject) a besoin d'un Rigidbody");
         }
     }
     private void OnMouseDown()
     {
+        if (pv.Owner != PhotonNetwork.LocalPlayer)
+        {
+            pv.TransferOwnership(PhotonNetwork.LocalPlayer);
+        }
         mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
         mOffset = gameObject.transform.position - GetMouseWorldPos();
         rb.isKinematic = true;
@@ -49,4 +59,15 @@ public class DragableObject : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(rb.isKinematic);
+        }
+        else
+        {
+            rb.isKinematic = (bool)stream.ReceiveNext();
+        }
+    }
 }
